7085437e774bc1f9ea32b24566367054240800cb
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 00:42:25 +0530

    Check Employee wage for 100 hours & 20 days a month

--- EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
+++ EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
@@ -7,15 +7,18 @@ public class Emp_Wage_Computation_prob {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 10;
 	
 	public static void main(String[] args) {
 		// Variables
-		int empHrs = 0;
-		int empWage = 0;
 		int totalEmpWage = 0;
+		int totalWorkingDays = 0;
+		int totalEmpHrs = 0;
 		// Computation
 		System.out.println("Welcome to Employee Wage computation Program.");
-		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			int empHrs = 0;
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			    	case IS_FULL_TIME:
@@ -30,7 +33,8 @@ public class Emp_Wage_Computation_prob {
 					System.out.println("Employee is Abscent.");
 					empHrs = 0;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpHrs += empHrs;
+			int empWage = empHrs * EMP_RATE_PER_HOUR;
 			totalEmpWage += empWage;
 			System.out.println("Emp Wage: " + empWage);
 		}
