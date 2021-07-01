9d8424cf856e92b206398bfc209c7d3f5a2d257e
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 00:18:45 +0530

    Welcome to Employee Wage Computation Program

---
+++ EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
@@ -0,0 +1,9 @@
+package Emp_Wage_Problem;
+
+public class Emp_Wage_Computation_prob {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage computation Program.");
+	}
+
+}
