1e3a0e5f3a336ac42d5e7dd24fc062feb583f53e
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 00:44:15 +0530

    Check Employee wage for 100 hours & 20 days a month

--- EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
+++ EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
@@ -7,7 +7,7 @@ public class Emp_Wage_Computation_prob {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 10;
+	public static final int MAX_HRS_IN_MONTH = 100;
 	
 	public static void main(String[] args) {
 		// Variables
