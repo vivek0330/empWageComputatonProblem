e306f2a4e2a747f5def017db6a14b232f7d90d12
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 00:21:18 +0530

    Check Employee Present Or Abscent

--- EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
+++ EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
@@ -3,7 +3,21 @@ package Emp_Wage_Problem;
 public class Emp_Wage_Computation_prob {
 	
 	public static void main(String[] args) {
+		
+		// Constant
+		int IS_FULL_TIME = 1;
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
 		System.out.println("Welcome to Employee Wage computation Program.");
+		if (empCheck == IS_FULL_TIME) 
+		{
+			System.out.println("Employee is Present.");
+		}
+		else
+		{
+			System.out.println("Employee is Abscent.");
+		}
+		
 	}
 
 }
