7c69c371a3b3d9588482d28279d056679eda8d0e
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 00:27:42 +0530

    Check Employee Full & Part time wage

--- EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
+++ EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
@@ -5,21 +5,25 @@ public class Emp_Wage_Computation_prob {
 	public static void main(String[] args) {
 		
 		// Constant
-		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		// Variables
 		int empHrs = 0;
 		int empWage = 0;
 		// Computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		System.out.println("Welcome to Employee Wage computation Program.");
 		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present.");
+			System.out.println("Employee is Full time.");
 			empHrs = 8;
 		}
+		else if (empCheck == IS_PART_TIME) {
+			System.out.println("Employee is Part time.");
+			empHrs = 4;
+		}
 		else {
-			System.out.println("Employee is Abscent.");
-			empHrs = 0;
+			System.out.println("Employee is Abscent");
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage: " + empWage);
