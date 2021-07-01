81b410e096c290b94e81cbd6aa72ba52be60e79f
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 00:31:38 +0530

    Check Employee wage for month

--- EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
+++ EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
@@ -6,30 +6,35 @@ public class Emp_Wage_Computation_prob {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 	
 	public static void main(String[] args) {
 		// Variables
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 		// Computation
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		System.out.println("Welcome to Employee Wage computation Program.");
-		switch (empCheck) {
-		    	case IS_FULL_TIME:
-		    		System.out.println("Employee is Full time.");
-				empHrs = 8;
-				break;
-			case IS_PART_TIME: 
-				System.out.println("Employee is Part time.");
-				empHrs = 4;
-				break;
-			default:
-				System.out.println("Employee is Abscent.");
-				empHrs = 0;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			    	case IS_FULL_TIME:
+			    	System.out.println("Employee is Full time.");
+					empHrs = 8;
+					break;
+				case IS_PART_TIME: 
+					System.out.println("Employee is Part time.");
+					empHrs = 4;
+					break;
+				default:
+					System.out.println("Employee is Abscent.");
+					empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage: " + empWage);
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
-		
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 
 }
