538f3d9f1aa9900c728574e793847e7ef09bfccf
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 00:24:50 +0530

    Check Employee Daily wage

--- EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
+++ EmpWageProblem/src/Emp_Wage_Problem/Emp_Wage_Computation_prob.java
@@ -6,17 +6,23 @@ public class Emp_Wage_Computation_prob {
 		
 		// Constant
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
 		// Computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		System.out.println("Welcome to Employee Wage computation Program.");
-		if (empCheck == IS_FULL_TIME) 
-		{
+		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present.");
+			empHrs = 8;
 		}
-		else
-		{
+		else {
 			System.out.println("Employee is Abscent.");
+			empHrs = 0;
 		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 		
 	}
 
